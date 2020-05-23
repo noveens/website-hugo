c78b50749df76ec2131854152d3e069b588df87b
noveens <noveen.sachdeva@research.iiit.ac.in>
2020-05-23 23:01:06 +0530

    Updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,14 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+git clone https://github.com/noveens/noveens.github.io.git
+rm -rf noveens.github.io/*
+mv public/* noveens.github.io/
+
 # Go To Public folder
-cd public
+cd noveens.github.io
+echo "www.noveens.com" > CNAME
+
 # Add changes to git.
 git add .
 
@@ -17,8 +23,11 @@ if [ $# -eq 1 ]
   fi
   git commit -m "$msg"
 
-  # Push source and build repos.
-  git push origin master
+# Push source and build repos.
+git push origin master
 
-  # Come Back up to the Project Root
-  cd ..
+# Come Back up to the Project Root
+cd ..
+
+# Delete stuff
+rm -rf public noveens.github.io
